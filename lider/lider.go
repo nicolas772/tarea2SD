02ab16ae63a861e4874717e779ad09bf891e396f
Nicolas Araya Urrutia <nicolas.arayau@sansano.usm.cl>
2021-11-05 21:37:14 -0300

    agrego interacion por terminal a lider.go

--- lider/lider.go
+++ lider/lider.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	"log"
+	"os"
+	"strings"
 	"time"
 
 	pb "example.com/go-msgs-grpc/msgs"
@@ -20,29 +24,49 @@ func failOnError(err error, msg string) {
 	}
 }
 
+func printmenuInicial() string {
+	menu :=
+		`
+		Bienvenido al juego del calamar (prueba Lider - Pozo)
+		[ 1 ] Pedir Monto acumulado al pozo
+		[ 2 ] Eliminar un jugador
+		[ otro ] Salir
+		Selecciona una opcion
+	`
+	fmt.Print(menu)
+	reader := bufio.NewReader(os.Stdin)
+
+	entrada, _ := reader.ReadString('\n')          // Leer hasta el separador de salto de línea
+	eleccion := strings.TrimRight(entrada, "\r\n") // Remover el salto de línea de la entrada del usuario
+	return eleccion
+}
+func pedirNombreJugador() string {
+	menu := "Escriba el nombre del jugador\n"
+
+	fmt.Print(menu)
+	reader := bufio.NewReader(os.Stdin)
+
+	entrada, _ := reader.ReadString('\n')          // Leer hasta el separador de salto de línea
+	eleccion := strings.TrimRight(entrada, "\r\n") // Remover el salto de línea de la entrada del usuario
+	return eleccion
+}
+func pedirRondaJugador() string {
+	menu := "Escriba la ronda en la que fue eliminado el jugador (por ejemplo, ronda 1)\n"
+
+	fmt.Print(menu)
+	reader := bufio.NewReader(os.Stdin)
+
+	entrada, _ := reader.ReadString('\n')          // Leer hasta el separador de salto de línea
+	eleccion := strings.TrimRight(entrada, "\r\n") // Remover el salto de línea de la entrada del usuario
+	return eleccion
+}
+
 func main() {
-
-	//Conexion con Pozo gRPC
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-	c := pb.NewPozoManagementClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	r, err := c.GetMontoAcumulado(ctx, &pb.NewMontoAcumulado{})
-	if err != nil {
-		log.Fatalf("No se pudo obtener el monto acumulado: %v", err)
-	}
-	log.Printf("monto acumulado: %d", r.GetMontoActual())
-
 	//Conexion con Pozo Rabbitmq
 
 	conn_pozo, err := amqp.Dial("amqp://admin:test@10.6.40.194:5672/qa1")
 	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+	defer conn_pozo.Close()
 
 	ch, err := conn_pozo.Channel()
 	failOnError(err, "Failed to open a channel")
@@ -58,16 +82,49 @@ func main() {
 	)
 	failOnError(err, "Failed to declare a queue")
 
-	body := "Jugador 1 Ronda 1" //este es el mensaje que se envia por rabbit
-	err = ch.Publish(
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		})
-	failOnError(err, "Failed to publish a message")
+	//Conexion con Pozo gRPC
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewPozoManagementClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	//Programa
+
+	flag := 1
+
+	for flag == 1 {
+		eleccion := printmenuInicial()
+
+		if eleccion == "1" {
+			r, err := c.GetMontoAcumulado(ctx, &pb.NewMontoAcumulado{})
+			if err != nil {
+				log.Fatalf("No se pudo obtener el monto acumulado: %v", err)
+			}
+			log.Printf("Monto acumulado: %d", r.GetMontoActual())
+		}
+		if eleccion == "2" {
+			nombre := pedirNombreJugador()
+			ronda := pedirRondaJugador()
+			body := nombre + " " + ronda //este es el mensaje que se envia por rabbit
+			err = ch.Publish(
+				"",     // exchange
+				q.Name, // routing key
+				false,  // mandatory
+				false,  // immediate
+				amqp.Publishing{
+					ContentType: "text/plain",
+					Body:        []byte(body),
+				})
+			failOnError(err, "Failed to publish a message")
+
+		} else {
+			log.Printf("Hasta Pronto!")
+			flag = 0
+		}
+	}
 
 }
